8064421e78bad3fa022b0643c6b5d3efad0033d9
Kazunari OKUDA <saori.cube@gmail.com>
2016-11-25 10:04:53 +0100

    Set boards#show to root url

--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class BoardsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get boards_show_url
-    assert_response :success
-  end
-
 end
